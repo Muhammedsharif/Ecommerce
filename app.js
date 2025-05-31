c703498111484c8cfdf0d02d5e37d7b67b032021
Muhammedsharif <shanusharif786@gmail.com>
2025-05-31 21:24:16 +0530

    view engine set up

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express=require("express")
 const app=express()
+const path=require("path")
 const env=require("dotenv").config()
-const db=require('./config/db')
+const db=require("./config/db")
+const userRouter=require("./routes/userRouter")
 db()
 
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine","ejs")
+app.use(express.static("public"));
+
+
+app.use("/",userRouter)
+
 app.listen(process.env.PORT,()=>console.log("Server running"))
+
+module.exports=app
---
+++ controllers/user/userController.js
@@ -0,0 +1,35 @@
+
+const pageNotFound=async (req,res)=>{
+
+    try{
+    
+      res.render("user/page-404")
+    
+    }catch(error){
+        res.redirect("/pageNotFound")
+        console.log("error")
+    }
+
+}
+
+
+
+
+
+const loadHomepage = async (req,res)=>{
+
+    try{
+        
+        return res.render("user/home")
+
+    }catch(error){
+        console.log("Home page not found")
+        res.status(500).send("Server error")
+    }
+
+}
+
+module.exports = {
+    loadHomepage,
+    pageNotFound,
+}
